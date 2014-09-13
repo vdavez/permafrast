3120437c2844b6d951d0f35f6faa58ec02812729
Ben Dixon <ben@talkingquickly.co.uk>
2014-09-13 17:12:04 +0100

    cleans up formatting

--- app.rb
+++ app.rb
@@ -10,10 +10,24 @@ get '/' do
 end
 
 get '/json/:vol/:reporter/:page' do
-  ::Fastcase::Client.new(ENV["FASTCASE_API_TOKEN"]).public_link(volume: params["vol"], reporter: params["reporter"], page: params["page"]).to_json
+  ::Fastcase::Client.new(
+    ENV["FASTCASE_API_TOKEN"]
+  ).public_link(
+    volume: params["vol"],
+    reporter: params["reporter"],
+    page: params["page"]
+  ).to_json
 end
 
 get '/info/:vol/:reporter/:page' do
-  out = JSON.parse(::Fastcase::Client.new(ENV["FASTCASE_API_TOKEN"]).public_link(volume: params["vol"], reporter: params["reporter"], page: params["page"]))
+  out = JSON.parse(
+    ::Fastcase::Client.new(
+      ENV["FASTCASE_API_TOKEN"]
+    ).public_link(
+      volume: params["vol"],
+      reporter: params["reporter"],
+      page: params["page"]
+    )
+  )
   erb :app, :locals => {"out"=>out}
 end
