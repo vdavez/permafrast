768ab88a90e0fafda7514ee0c5d794a6b1788017
Alan deLevie <adelevie@gmail.com>
2014-09-14 18:18:30 -0400

    Added test coverage of Sinatra app.
    Added rspec and ruby version to travis.yml
    
    added test instructions to readme
    
    Tweak travis.yml to use quotes for ruby version
    
    Adding pg capabilities to travis
    
    Add special db.yml just for travis
    
    Trying again at travis whitespace issue
    
    trying a 3rd time with travis
    
    add rake db prepare to travis
    
    Replacing pg with sqlite in travis
    
    adding a rake:db:migrate to travis because why not
    
    Added rake to my gemfile as travis suggests
    
    I will be equal parts pleased and annoyed if this works.
    
    remove bundle exec. because that always ends well.
    
    remove sqlite3, update gitignore
    
    let's give it a go

---
+++ spec/app_spec.rb
@@ -0,0 +1,90 @@
+require_relative '../app'
+require 'rspec'
+require 'rack/test'
+require 'json'
+require 'nokogiri'
+
+
+RSpec.configure do |config|
+end
+
+module SpecConstants
+  VOLUME        = 600
+  REPORTER      = 'F.3d'
+  PAGE          = 642
+  URL           = "https://apps.fastcase.com/Research/Public/ExViewer.aspx?LTID=%2fGLQLe%2fDaGym1PLr4VyFrNW1GRW%2fFszkp5OJNGHvwRPnb22Q5oSdo7jrjk8wbX8Q"
+  FULL_CITATION = "Comcast Corp. v. Fed. Commc'ns Comm'n, 600 F.3d 642 (D.C. Cir., 2010)"
+end
+
+describe 'The HelloWorld App', type: :feature do
+  include Rack::Test::Methods
+  
+  before do
+    # create a fake CallCache instance
+    cc = CallCache.new
+    cc.volume        = SpecConstants::VOLUME
+    cc.reporter      = SpecConstants::REPORTER
+    cc.page          = SpecConstants::PAGE
+    cc.url           = SpecConstants::URL
+    cc.full_citation = SpecConstants::FULL_CITATION
+    
+    fake_response = cc
+    cacher_instance = {}
+    
+    # mock instance of Cacher
+    allow(cacher_instance).to(receive(:cache!)).and_return(fake_response)
+    
+    # return mocked instance of Cacher with a mock of its `new` method
+    allow(Cacher).to(receive(:new)).and_return(cacher_instance)
+  end
+
+  def app
+    App
+  end
+
+  it 'has a homepage' do
+    get '/'
+    # test http status code
+    expect(last_response.status).to(eq(200))
+    
+    page = Nokogiri::HTML(last_response.body)
+    # ensure Permafrast text is at the top of the page
+    expect(page.at_css('body > h1:nth-child(1)').text).to(eq('Permafrast'))
+  end
+  
+  it 'gets HTML' do
+    url = "#{SpecConstants::VOLUME}/#{SpecConstants::REPORTER}/#{SpecConstants::PAGE}"
+    get url, nil, {'HTTP_ACCEPT' => "text/html"}
+    
+    # test http status code
+    expect(last_response.status).to(eq(200))
+    
+    page = Nokogiri::HTML(last_response.body)
+    a_tag = page.at_css('body > a')
+    
+    # test link text
+    expect(a_tag.text).to(eq(SpecConstants::FULL_CITATION))
+    # test link href
+    expect(a_tag.attributes['href'].value).to(eq(SpecConstants::URL))
+  end
+    
+  it 'gets JSON' do
+    url = "#{SpecConstants::VOLUME}/#{SpecConstants::REPORTER}/#{SpecConstants::PAGE}.json"
+    get url, nil, {'HTTP_ACCEPT' => "application/json"}
+    
+    # test http status code
+    expect(last_response.status).to(eq(200))
+        
+    json = JSON.parse(last_response.body)
+    expected = {
+      "volume"=>SpecConstants::VOLUME, 
+      "reporter"=>SpecConstants::REPORTER, 
+      "page"=>SpecConstants::PAGE, 
+      "url"=>SpecConstants::URL, 
+      "full_citation"=>SpecConstants::FULL_CITATION
+    }
+    
+    # test the json response
+    expect(json).to(include(expected))
+  end
+end
